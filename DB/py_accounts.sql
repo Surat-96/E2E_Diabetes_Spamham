95f3cb8a424580cff919e17831696022b6494763
Surat-96 <surat.banerjee@gmail.com>
2020-07-13 10:56:24 +0530

    first commit

---
+++ DB/py_accounts.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2020 at 09:08 AM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `py_accounts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `age` int(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `age`, `email`, `password`) VALUES
+(1, 'Surat Banerjee', 24, 'surat@gmail.com', 'surat'),
+(2, 'Moumita Banerjee', 30, 'soumayan.ban@gmail.com', 'moumita');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask,render_template,redirect,url_for,request
+import pickle
+import numpy as np
+
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+
+app = Flask(__name__)
+
+#diabetes model read
+filename = open('Diabetes/diabetespredictmodel.pkl', 'rb')
+model = pickle.load(filename)
+filename.close()
+
+#spamham model read
+filename = open('Spam_Ham/spam-sms-mnb-model.pkl', 'rb')
+classifier = pickle.load(filename)
+files = open('Spam_Ham/cv-transform.pkl','rb')
+cv = pickle.load(files)
+filename.close()
+files.close()
+
+
+@app.route('/')
+def index():
+	return render_template('home.html')
+
+@app.route('/home')
+def home():
+	return render_template('home.html')
+    
+@app.route('/diapredict', methods=['GET','POST'])
+def diapredict():
+    if request.method == 'POST':
+        na = request.form['na']
+        pr = int(request.form['pr'])
+        gl = int(request.form['gl'])
+        bp = int(request.form['bp'])
+        st = int(request.form['st'])
+        ins = int(request.form['in'])
+        bm = float(request.form['bm'])
+        dp = float(request.form['dp'])
+        ag = int(request.form['ag'])
+        
+        data = np.array([[pr,gl,bp,st,ins,bm,dp,ag]])
+        my_prediction = model.predict(data)
+        my_prediction_proba = model.predict_proba(data)[0][1]
+        
+        return render_template('diashow.html',name=na,prediction=my_prediction,proba=my_prediction_proba)
+    return render_template('dia.html')
+
+@app.route('/sphampredict',methods=['GET','POST'])
+def sphampredict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = classifier.predict(vect)
+        return render_template('sphamshow.html', prediction=my_prediction)
+    return render_template('spham.html')
+
+        
+if __name__ == '__main__':
+	app.run(debug=True)
